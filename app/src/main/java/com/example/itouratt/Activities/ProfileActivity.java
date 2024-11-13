ebd76a26feca8bc4a6cb283f9b397f0379846d0c
João Pedro <developerjotape@gmail.com>
2024-11-13 09:36:45 -0300

    New: Ticket viewholder

--- app/src/main/java/com/example/itouratt/Activities/ProfileActivity.java
+++ app/src/main/java/com/example/itouratt/Activities/ProfileActivity.java
@@ -1,6 +1,8 @@
 package com.example.itouratt.Activities;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,11 @@ public class ProfileActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_profile);
+    }
+
+    public void editProfile(View view) {
+        Intent in = new Intent(ProfileActivity.this, EditProfileActivity.class);
+        startActivity(in);
     }
 
 }
--- app/src/main/java/com/example/itouratt/Activities/TicketsActivity.java
+++ app/src/main/java/com/example/itouratt/Activities/TicketsActivity.java
@@ -1,6 +1,19 @@
 package com.example.itouratt.Activities;
 
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.itouratt.R;
+
 public class TicketsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        //setContentView(R.layout.activity_intro);
+    }
+
 }
